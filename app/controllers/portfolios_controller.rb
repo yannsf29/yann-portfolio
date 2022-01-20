18b2608778cd5501491a0d4f669be6e667127ffa
yannsf29 <yann.crennsf@gmail.com>
2022-01-19 20:17:35 -0800

    portfolios updated

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,9 +2,25 @@ class PortfoliosController < ApplicationController
 
     def index 
         @portfolio_items = Portfolio.all  
-        
     end
-       
+    def new
+        @portfolio_item = Portfolio.new
+    end 
+ 
+    def create
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @portfolio_item.save
+            format.html { redirect_to @portfolio_path, notice: "Your Portfolio item is Now Live." }
+            
+          else
+            format.html { render :new }
+            
+          end
+        end
+      end
+
        
 end
 
