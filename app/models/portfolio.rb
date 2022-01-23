98e3da4416c7fbbe3e5b395e2677af20a39ebf3d
yannsf29 <yann.crennsf@gmail.com>
2022-01-23 14:47:29 -0800

    Implemented logout functionality and dynamic view rendering for auth

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,15 +1,15 @@
 class Portfolio < ApplicationRecord
     
     has_many :technologies
-    include Placeholder
+    accepts_nested_attributes_for :technologies, 
+                                  reject_if: lambda { |attrs| attrs['name'].blank? }
     
-
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
-  accepts_nested_attributes_for :technologies, 
-                               reject_if: lambda { |attrs| attrs['name'].blank? }
+  
 
  def self.angular 
- where(subtitle: 'Java Script')
+ where(subtitle: 'Angular')
  end
 
  after_initialize :set_defaults
